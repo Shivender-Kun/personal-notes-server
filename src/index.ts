d3bd5f48b53759b78faaf216d8fbb8a2e69cbfe0
Shivender-Kun <shivenderkumar761@gmail.com>
2025-06-07 12:11:51 +0530

    cors origins list handling through env

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,31 @@
+import { ALLOWED_ORIGINS } from "./constants";
 import { errorHandler } from "./middlewares";
 import cookieParser from "cookie-parser";
 import { PORT } from "./config";
 import express from "express";
-import routes from "./routes"; // Import routes and apply them to the app
+import routes from "./routes";
 import morgan from "morgan";
 import cors from "cors";
 import path from "path";
 import DB from "./db";
 
-// Create an Express app instance and configure it to use middleware for parsing JSON request bodies
+// Create an instance of the Express application
 const app = express();
 
+// Middleware to parse cookies from incoming requests
 app.use(cookieParser());
 
 // Enable CORS for all routes in the application
 app.use(
   cors({
     origin: (origin, callback) => {
-      const allowedOrigins = [
-        "http://localhost:3000",
-        "https://pn-web.netlify.app",
-        "https://notes.shivender.pro",
-      ];
-
-      if (!origin || allowedOrigins.includes(origin)) {
+      if (!origin || ALLOWED_ORIGINS.includes(origin)) {
         // Allow requests with no origin (like server-to-server or health checks)
         callback(null, true);
       } else {
         callback(new Error("Not allowed by CORS"));
       }
     },
-    // origin: "http://localhost:3000",
     credentials: true,
   })
 );
@@ -50,10 +45,9 @@ app.use("/api/notes", routes.Note);
 app.use("/api/labels", routes.Label);
 app.use("/api/users", routes.User);
 
+// Handle errors
 app.use(errorHandler);
 
-// Connect to the database before starting the server
 DB.connect();
 
-// Start the server and log a message to the console when it's listening
 app.listen(PORT, () => console.log(`Server started on Port ${PORT}`));
